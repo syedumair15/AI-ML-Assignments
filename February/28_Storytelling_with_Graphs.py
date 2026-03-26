9dfe047a18bc00cb4f8097f5104ec9f0b1a1b414
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:21:24 +0530

    28_Storytelling_with_Graphs.py

--- February/28_Storytelling_with_Graphs.py
+++ February/28_Storytelling_with_Graphs.py
@@ -1,1 +1,16 @@
 # 28_Storytelling_with_Graphs.py
+import matplotlib.pyplot as plt
+
+subjects = ["Math","Science","English","Computer","Social"]
+marks = [85,78,88,92,74]
+
+scores = [45,55,67,72,80,85,90,95,60,70]
+
+plt.bar(subjects, marks)
+plt.show()
+
+plt.pie(marks, labels=subjects, autopct='%1.1f%%')
+plt.show()
+
+plt.hist(scores)
+plt.show()
