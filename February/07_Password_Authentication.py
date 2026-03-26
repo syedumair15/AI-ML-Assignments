b8ed8330d32da6e20ab5937d1d00d62f17af018b
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:16:57 +0530

    07_Password_Authentication.py

--- February/07_Password_Authentication.py
+++ February/07_Password_Authentication.py
@@ -1,1 +1,35 @@
 # 07_Password_Authentication.py
+# Assignment – Password Authentication
+
+import re
+
+def is_strong(password):
+    if len(password) < 8:
+        return False
+    if not re.search("[A-Z]", password):
+        return False
+    if not re.search("[a-z]", password):
+        return False
+    if not re.search("[0-9]", password):
+        return False
+    if not re.search("[!@#$%^&*]", password):
+        return False
+    return True
+
+password = input("Create password: ")
+
+while not is_strong(password):
+    print("Weak password. Try again.")
+    password = input("Create password: ")
+
+print("Password created successfully")
+
+attempts = 3
+while attempts > 0:
+    user_input = input("Enter password: ")
+    if user_input == password:
+        print("Login successful")
+        break
+    else:
+        attempts -= 1
+        print("Wrong password. Attempts left:", attempts)
