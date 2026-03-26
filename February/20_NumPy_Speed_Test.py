b8c5f64caf9fe41e2de287c7ce9c6b3010da4c4d
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:19:43 +0530

    20_NumPy_Speed_Test.py

--- February/20_NumPy_Speed_Test.py
+++ February/20_NumPy_Speed_Test.py
@@ -1,1 +1,15 @@
 # 20_NumPy_Speed_Test.py
+import time
+import numpy as np
+
+size = 1000000
+
+start = time.time()
+lst = list(range(size))
+lst = [x*2 for x in lst]
+print("List time:", time.time() - start)
+
+start = time.time()
+arr = np.arange(size)
+arr = arr * 2
+print("NumPy time:", time.time() - start)
