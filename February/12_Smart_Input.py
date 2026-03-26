2d11c54bf97b6cb0850b99cd6125107f1a351718
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:18:12 +0530

    12_Smart_Input.py

--- February/12_Smart_Input.py
+++ February/12_Smart_Input.py
@@ -1,1 +1,17 @@
 # 12_Smart_Input.py
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+hobby = input("Enter hobby: ")
+
+if age < 13:
+    category = "Child"
+elif age < 20:
+    category = "Teenager"
+elif age < 60:
+    category = "Adult"
+else:
+    category = "Senior"
+
+print("Hello", name)
+print("Category:", category)
+print("Hobby:", hobby)
