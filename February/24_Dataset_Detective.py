5d77d0f975bb5328d7bd2048b97c716665825bd5
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:20:32 +0530

    24_Dataset_Detective.py

--- February/24_Dataset_Detective.py
+++ February/24_Dataset_Detective.py
@@ -1,1 +1,11 @@
 # 24_Dataset_Detective.py
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+
+print(df.head())
+
+num = df.select_dtypes(include="number")
+print(num.mean().idxmax())
+
+print(df.isnull().sum())
