7d46c7bec6d5183ffcc57b6517f2d0e0fe41adbc
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:23:14 +0530

    09_House_Price_Predictor.py

--- March/09_House_Price_Predictor.py
+++ March/09_House_Price_Predictor.py
@@ -1,1 +1,11 @@
 # 09_House_Price_Predictor.py
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+X = np.array([500,800,1000]).reshape(-1,1)
+y = [100000,150000,200000]
+
+model = LinearRegression()
+model.fit(X,y)
+
+print(model.predict([[900]]))
