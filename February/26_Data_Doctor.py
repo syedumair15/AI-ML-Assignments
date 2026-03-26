89bde4e078789ca30643f6c2287f5fba57cab544
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:21:01 +0530

    26_Data_Doctor.py

--- February/26_Data_Doctor.py
+++ February/26_Data_Doctor.py
@@ -1,1 +1,14 @@
 # 26_Data_Doctor.py
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+
+df.fillna(df.mean(numeric_only=True), inplace=True)
+df.fillna("unknown", inplace=True)
+
+df.drop_duplicates(inplace=True)
+
+for col in df.select_dtypes(include="object"):
+    df[col] = df[col].str.lower().str.strip()
+
+print(df.head())
