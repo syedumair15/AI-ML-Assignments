7908c815d1de509c4f3b49c1e4121f2d0230010c
Syed Umair <syedumair1524@gmail.com>
2026-03-26 16:25:01 +0530

    11_Customer_Segmentation.py

--- March/11_Customer_Segmentation.py
+++ March/11_Customer_Segmentation.py
@@ -1,1 +1,11 @@
 # 11_Customer_Segmentation.py
+from sklearn.cluster import KMeans
+import pandas as pd
+
+data = {"Income":[20,30,40,50],"Score":[30,60,70,90]}
+df = pd.DataFrame(data)
+
+k = KMeans(n_clusters=2)
+df["Cluster"] = k.fit_predict(df)
+
+print(df)
